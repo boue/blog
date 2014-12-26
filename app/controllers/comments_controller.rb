a32dbb9da5475199d13a674d446448cc412ff5a8
Germain Boue <boue.germain@gmail.com>
2014-12-25 21:10:52 -0600

    Fixed bug whereas you could not be able to see the body of the comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment].permit(:name, :comment))
+    @comment = @post.comments.create(params[:comment].permit(:name, :body))
 
     redirect_to post_path(@post)
   end
@@ -13,5 +13,4 @@ class CommentsController < ApplicationController
 
     redirect_to post_path(@post)
   end
-
 end
